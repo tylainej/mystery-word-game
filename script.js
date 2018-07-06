f72764aa9db33511bb95d7c7f3bf8e5a73df95b4
Tylaine Jeffrey <tylainej@gmail.com>
2018-07-06 12:06:18 -0400

    changed back to underscores

--- script.js
+++ script.js
@@ -4,28 +4,36 @@ const words = ['monday','tuesday','wednesday','thursday','friday'];
 let random = Math.floor(Math.random() * words.length);
 let chosenWord = words[random];
 let underscores = [];
-//create underscores based on length of word
-function Underscores () {
-let newEl = document.querySelector('#underScore');
-let typeField = document.createElement('div');
-typeField.className = "guessInput";
-newEl.appendChild(typeField);
-    for(let i = 0; i < chosenWord.length; i += 1){
- console.log(chosenWord);
-    underscores.push(chosenWord[typeField]);
-}
-return underscores;
+//create divs based on length of word
 
+// let newEl = document.querySelector('#underScore');
+// let newDiv = document.createElement('div');
+// newDiv.className = "guessInput";
+// newEl.appendChild(newDiv);
+
+// create new p element to append to new div class guessInput
+// let newPel = document.createElement('p');
+// newPel.className = "newPel";
+// newDiv.appendChild(newPel);
+// loop the letters of chosenWord into the newly created div
+
+function Underscores () {
+    for(let i = 0; i < chosenWord.length; i += 1){
+        
+        underscores.push('_');
+    }
+        return underscores;
 }
 console.log(Underscores());
+
 function Alphabet(){
     let y = document.querySelector('#clue');
     document.style.display = 'none';
     y.addEventListener('keypress', Alphabet);
 
 }
-
-let x = document.querySelector('input').innerHTML = underscores;
+// when you had underscore you used this to push them to the html 
+  let x = document.querySelector('#underScore').innerHtml = underscores;
 
 // get users' guess
 document.addEventListener('keypress', (evt) =>{
